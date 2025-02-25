a18ee58ab41a2e72bca8de95a43a97e5b1885297
SpilMand <prohorychev.semen@gmail.com>
2025-02-25 19:47:04 +0300

    FEAT - добавлен попап заметки

--- src/components/sections/s-notes/s-notes.vue
+++ src/components/sections/s-notes/s-notes.vue
@@ -3,16 +3,26 @@
     <div v-if="notes" class="s-notes__content">
       <mNote v-for="(note, index) in notes.data" :key="index" :data="note" />
     </div>
+    <aButton class="s-notes__add" icon="src/assets/images/add.svg" @click="popupStore.noteCloseOpen()" />
+    <sPopup
+      v-show="popupStore.isNoteOpened"
+    >
+
+    </sPopup>
   </section>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
 import mNote from '@/components/molecules/m-note/m-note.vue';
+import aButton from '@/components/atoms/a-button/a-button.vue';
+import sPopup from '../s-popup/s-popup.vue';
 import { getNotes } from '@/api/api';
 import { useAuthStore } from '@/stores/auth';
+import { usePopupStore } from '@/stores/popup';
 
 const authStore = useAuthStore();
+const popupStore = usePopupStore();
 
 const notes = ref();
 
--- src/stores/popup.ts
+++ src/stores/popup.ts
@@ -3,15 +3,20 @@ import { defineStore } from 'pinia'
 
 export const usePopupStore = defineStore('popup', () => {
   const isOpened = ref(false);
+  const isNoteOpened = ref(false);
   const isRegister = ref(false);
 
   function closeOpen() {
     isOpened.value = !isOpened.value;
   }
 
+  function noteCloseOpen() {
+    isNoteOpened.value = !isNoteOpened.value;
+  }
+
   function changeType() {
     isRegister.value = !isRegister.value;
   }
 
-  return { isOpened, isRegister, closeOpen, changeType }
+  return { isOpened, isRegister, isNoteOpened, closeOpen, noteCloseOpen, changeType }
 })
