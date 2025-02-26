be05a376931e14ce21bbec8314e4f6ca8b5a3d11
SpilMand <prohorychev.semen@gmail.com>
2025-02-26 13:34:06 +0300

    config fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/notes/'
-    : '/'
-}
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/notes/' : '/',
+});
